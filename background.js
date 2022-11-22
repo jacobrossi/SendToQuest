98e3a07f7e4b3ee57ea074c4e41dee6ca809a27e
jacobrossi <gtjrossi@gmail.com>
2022-11-21 18:07:28 -0800

    Add action button support for opening current tab

--- background.js
+++ background.js
@@ -1,25 +1,41 @@
-// Create the context menu item
+// Create the context menu option
 chrome.runtime.onInstalled.addListener(async () => {
    chrome.contextMenus.create({
-      id: "sendtoquest",
-      title: "Send to Meta Quest",
-      type: 'normal',
-      contexts: ['link'],
-    });
+        id: "sendtoquest",
+        title: "Send to Meta Quest",
+        type: 'normal',
+        contexts: ['link'],
+   });
+});
+
+// Handle Action button invocation
+chrome.action.onClicked.addListener(async (tab) => {
+    openSendToQuestPopup(tab.url);
 });
 
 // Open web flow to send selected link to headset
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-  let sendToQuestUrl = new URL("https://oculus.com/open_url/")
-  sendToQuestUrl.searchParams.set('url', info.linkUrl)
-  //Open flow in a new tab
-  //chrome.tabs.create({ url: url.href, index: tab.index + 1 });
-  //Open flow in a popup
-  chrome.windows.create({
-    focused: true,
-    height: 500,
-    width: 400,
-    type: "popup",
-    url: sendToQuestUrl.href
-  })
+    openSendToQuestPopup(info.linkUrl);
 });
+
+function openSendToQuestPopup(linkUrl) {
+    // Confirm it's an http/https link and not something else like chrome://
+    if (!linkUrl.startsWith("https://") && !linkUrl.startsWith("http://")) {
+        return;
+    }
+
+    let sendToQuestUrl = new URL("https://oculus.com/open_url/")
+    sendToQuestUrl.searchParams.set('url', linkUrl)
+    // Open flow in a new tab
+    //chrome.tabs.create({ url: url.href, index: tab.index + 1 });
+    // Open flow in a popup
+    chrome.windows.create({
+        focused: true,
+        height: 500,
+        width: 400,
+        left: 300,
+        top: 100,
+        type: "popup",
+        url: sendToQuestUrl.href
+    });
+}
